a00ae71b120609049c2bfb5347c066c01c54e711
Keith Summers <pikimix@gmail.com>
2014-05-05 14:16:55 +0100

    scan

--- round5/test.rb
+++ round5/test.rb
@@ -4,13 +4,15 @@ class Test1 < Test::Unit::TestCase
 
 	def test_adding_single_item_totals_up
 		checkout = Checkout.new
-		checkout.scan
+		checkout.scan 'A99'
 		assert_equal checkout.total,50
 	end
 
 end
 
 class Checkout
+
+
 
 	def scan
 
