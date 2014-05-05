137e41ee35bf6bf00fb4c0ee11ece3f8749738d8
Keith Summers <pikimix@gmail.com>
2014-05-05 13:57:00 +0100

    first test single item A99

--- round5/test.rb
+++ round5/test.rb
@@ -4,11 +4,16 @@ class Test1 < Test::Unit::TestCase
 
 	def test_total_is_50_when_scan_A99
 		checkout = Checkout.new
+		checkout.scan( 'A99')
 		assert_equal checkout.total,50
 	end
 end
 
 class Checkout
+
+	def scan sku
+
+	end
 
 	def total
 		50
