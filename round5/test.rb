97659bd3e797e77f22174a16ca7dfe53c4ccdf68
Keith Summers <pikimix@gmail.com>
2014-05-05 13:44:37 +0100

    refactoring

--- round5/test.rb
+++ round5/test.rb
@@ -3,18 +3,12 @@ require 'test/unit'
 class Test1 < Test::Unit::TestCase
 
 	def test_total_of_empty_basket_is_zero
-		checkout = Checkout.new
+		checkout = Checkout.new({})
 		assert_equal checkout.total,0
 	end
 
-	def test_single_item_of_A99_in_basket
-		checkout = Checkout.new
-		checkout.scan "A99"
-		assert_equal checkout.total,50
-	end
-
 	def test_single_item_of_B15_in_basket
-		products = {'A99' => 50, 'B15' => 30}
+		products = {'B15' => 30}
 		checkout = Checkout.new(products)
 		checkout.scan "B15"
 		assert_equal checkout.total,30
@@ -24,7 +18,7 @@ end
 
 class Checkout
 
-	def initialize(products = {'A99' => 50, 'B15' => 30})
+	def initialize(products)
 		@total = 0
 		@products = products 
 	end 
