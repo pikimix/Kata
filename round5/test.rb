8685558ccdd0aa424eb1e9e1aaf774c81b23c7df
Keith Summers <pikimix@gmail.com>
2014-05-05 14:36:05 +0100

    test checout total

--- round5/test.rb
+++ round5/test.rb
@@ -3,10 +3,22 @@ require 'test/unit'
 class Test1 < Test::Unit::TestCase
 
 	def test_total_of_1_A99_is_50
-		
-		assert_equal total, 50
+		checkout = Checkout.new
+		assert_equal checkout.total, 50
 	end
 
 	def total
+		50
 	end
+
+end
+
+class Checkout
+
+
+	def total
+		50
+	end
+
+
 end
