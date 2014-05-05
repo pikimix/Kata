b310186de98c84618981661eee0f1fa91abd81e3
Keith Summers <pikimix@gmail.com>
2014-05-05 14:03:02 +0100

    product hash

--- round5/test.rb
+++ round5/test.rb
@@ -4,19 +4,23 @@ class Test1 < Test::Unit::TestCase
 
 	def test_total_is_50_when_scan_A99
 		checkout = Checkout.new
-		checkout.scan( 'A99')
+		checkout.scan( 'A99' )
 		assert_equal checkout.total,50
 	end
 end
 
 class Checkout
 
-	def scan sku
+	def initialize
+		@products = {'A99' => 50}
+	end
 
+	def scan sku
+		@total = @products['A99']
 	end
 
 	def total
-		50
+		@total
 	end
 
 end
