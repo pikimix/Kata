9491a7bf2524b5cc22887f333f4a558f474d287e
Keith Summers <pikimix@gmail.com>
2014-05-05 14:15:31 +0100

    product hash

--- round5/test.rb
+++ round5/test.rb
@@ -2,7 +2,20 @@ require 'test/unit'
 
 class Test1 < Test::Unit::TestCase
 
-	def test_something
-		assert_equal 0,0
+	def test_adding_single_item_totals_up
+		checkout = Checkout.new
+		
+		assert_equal checkout.total,50
 	end
+
+end
+
+class Checkout
+
+
+
+	def total
+		50
+	end
+
 end
