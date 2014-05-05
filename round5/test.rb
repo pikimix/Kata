564d51e09b34334d027e2e61474f1d0357438aba
Keith Summers <pikimix@gmail.com>
2014-05-05 14:28:54 +0100

    scan totals

--- round5/test.rb
+++ round5/test.rb
@@ -3,8 +3,25 @@ require 'test/unit'
 class Test1 < Test::Unit::TestCase
 
 	def test_scan_single_product
-		total = 50
-		assert_equal total,50
+		checkout = Checkout.new
+		checkout.scan "A99"
+		assert_equal checkout.total,50
+	end
+
+end
+
+class Checkout 
+
+	def initialize
+		@products = {"A99" => 50}
+	end
+
+	def scan sku
+		@total = 50
+	end
+
+	def total
+		@total
 	end
 
 end
